ee6ae7d0e540168ea2e76fb082d592e668a46971
Aimee <314552015@qq.com>
2018-04-21 15:25:04 +0800

    add user_loader

--- simpledu/app.py
+++ simpledu/app.py
@@ -1,8 +1,9 @@
 # coding:'utf-8'
 from flask import Flask, render_template
 from simpledu.config import configs
-from simpledu.models import db, Course
+from simpledu.models import db, Course, User
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 """app.py 用来创建工厂函数，专门用来创建app实力对象 """
 
@@ -20,10 +21,12 @@ def create_app(config):
 
 # 将配置好的app传给SQLAlchemy　去动态创建与数据库的映射 """
 
-    db.init_app(app)
+    #db.init_app(app)
 
-    Migrate(app, db)
+    #Migrate(app, db)
 
+    # 注册拓展 flask_login
+    register_extensions(app)
     register_blueprints(app)
 
     return app
@@ -36,4 +39,17 @@ def register_blueprints(app):
     app.register_blueprint(course)
     app.register_blueprint(admin)
 
+# 定义register_extensions函数，用于将Flask拓展注册到app
 
+def register_extensions(app):
+    db.init_app(app)
+    Migrate(app, db)
+# 从Flask_login导入LoginManager后，创建该类的实例对象
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+# 使用user_loader装饰器注册一个函数，用来告诉flask_login如何加载用户对象
+    @login_manager.user_loader
+    def user_loader(id):
+        return User.query.get(id)
+
+    login_manager.login_view = 'front.login'
--- simpledu/models.py
+++ simpledu/models.py
@@ -3,13 +3,15 @@
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 # db实例化对象不再传入　app对象
 # 创建数据库ORM对象
 db = SQLAlchemy()
 
 # 创建基类，方便以后创建的表使用
-class Base(db.Model):
+class Base(db.Model, UserMixin):
+    # 继承UserMixin 使用is_authenticated property() 判断用户是否登录状态
 
     """ 所有model的一个基类，默认添加了时间戳"""
     # 不要把这个表当做 Model类, 即不在数据库中创建该表,可以当做基类
