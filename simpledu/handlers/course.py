3d14c5a8ec997ebf5c5e732c39eeb516b1abd7ed
Aimee <314552015@qq.com>
2018-04-27 23:50:40 +0800

    add chapter.html

--- simpledu/handlers/course.py
+++ simpledu/handlers/course.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template
-from simpledu.models import Course
+from simpledu.models import Course, Chapter
+from flask_login import login_required
+
 
 course = Blueprint('course', __name__, url_prefix='/courses')
 
@@ -7,3 +9,9 @@ course = Blueprint('course', __name__, url_prefix='/courses')
 def detail(course_id):
     course = Course.query.get_or_404(course_id)
     return render_template('course/detail.html', course=course)
+
+@course.route('/<int:course_id>/chapters/<int:chapter_id>')
+@login_required
+def chapter(course_id, chapter_id):
+    chapter = Chapter.query.get_or_404(chapter_id)
+    return render_template('course/chapter.html', chapter=chapter)
