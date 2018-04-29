2d94b885396563226b013d36446858f22271eac6
Aimee <314552015@qq.com>
2018-04-29 17:26:12 +0800

    add edit & delete function

--- simpledu/handlers/admin.py
+++ simpledu/handlers/admin.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, current_app, redirect, url_for, flash
 from simpledu.decorators import admin_required
-from simpledu.models import Course
+from simpledu.models import Course, db
 from simpledu.forms import CourseForm
 admin = Blueprint('admin', __name__, url_prefix='/admin')
 
@@ -21,6 +21,7 @@ def courses():
             )
     return render_template('admin/courses.html', pagination=pagination)
 
+# 添加课程路由
 @admin.route('/courses/create', methods=['GET', 'POST'])
 @admin_required
 def create_course():
@@ -30,3 +31,25 @@ def create_course():
         flash('课程创建成功', 'sucess')
         return redirect(url_for('admin.courses'))
     return render_template('admin/create_courses.html', form=form)
+
+# 编辑课程路由
+@admin.route('/courses/<int:course_id>/edit', methods=['POST', 'GET'])
+@admin_required
+def edit_course(course_id):
+    course = Course.query.get_or_404(course_id)
+    form = CourseForm(obj=course)
+    if form.validate_on_submit():
+        form.update_course()
+        flash('课程修改成功', 'success')
+        return redirect(url_for('admin.courses'))
+    return render_template('admin/edit_courses.html', form=form, course=course)
+
+# 删除课程
+@admin.route('/courses/<int:course_id>/delete')
+@admin_required
+def delete_course(course_id):
+    course = Course.query.get_or_404(course_id)
+    db.session.delete(course)
+    db.session.commit()
+    flash('课程删除成功', 'success')
+    return redirect(url_for('admin.courses'))
