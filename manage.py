126f1cf48e607189b60544dc562e7d1a648954cc
Aimee <314552015@qq.com>
2018-05-23 11:42:36 +0800

    add system message

--- manage.py
+++ manage.py
@@ -4,8 +4,8 @@ from simpledu.app import create_app
 app = create_app('development')
 
 if __name__ == '__main__':
-    #app.run(host='0.0.0.0')
-    from gevent import pywsgi
-    from geventwebsocket.handler import WebSocketHandler
-    server = pywsgi.WSGIServer(('',5000), app, handler_class=WebSocketHandler)
-    server.serve_forever()
+    app.run(host='0.0.0.0')
+    #from gevent import pywsgi
+    #from geventwebsocket.handler import WebSocketHandler
+    #server = pywsgi.WSGIServer(('',5000), app, handler_class=WebSocketHandler)
+    #server.serve_forever()
--- simpledu/handlers/live.py
+++ simpledu/handlers/live.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template
 from simpledu.forms import LiveForm
+from .ws import redis
+from simpledu.forms import MessageForm
 
 live = Blueprint('live', __name__, url_prefix='/live')
 
@@ -7,3 +9,4 @@ live = Blueprint('live', __name__, url_prefix='/live')
 def index():
     form = LiveForm()
     return render_template('live/index.html', form=form)
+
